23a69825ea417bcfbd208fdf82ffa54644e927e3
testrigtech <info@testrigtechnologies.com>
2019-11-21 16:47:34 +0530

    first commit

---
+++ src/main/java/com/testrig/Hello_World/App.java
@@ -0,0 +1,13 @@
+package com.testrig.Hello_World;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
